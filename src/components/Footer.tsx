2c451b75eb9053be42e44880db586e36c88c6dec
sarthak <thakresarthak6@.com>
2024-12-05 18:40:05 +0530

    Footer Alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-[80vw] bg-[#ffffff] text-[#8c8c8c] py-4 fixed bottom-0 pl-10 pr-10">
-      <div className="container flex justify-between text-sm">
-        <div className="text-start">
+    <footer className="w-full bg-[#ffffff] text-[#8c8c8c] py-4 fixed bottom-0 left-0 right-0">
+      <div className="flex justify-between items-center w-full px-10 text-sm">
+        <div className="flex-1 text-center">
           Copyright © 2019, <span className="text-[#0275d8]">CRM</span> All rights reserved.
         </div>
-        <div className="item-center">
+        <div className="mr-20">
           Designed & Developed <span className="text-[#0275d8]">By iAssure International Technologies Pvt. Ltd.</span>
           Version 1.0.0
         </div>
